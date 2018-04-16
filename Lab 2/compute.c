713c36ff6b5f694aab9c95336831422833e2acd4
Zixiang Li <lzx97@outlook.com>
2018-04-15 17:53:55 -0700

    Implementing compute and measure

--- Lab 2/compute.c
+++ Lab 2/compute.c
@@ -1,3 +1,9 @@
+#include <math.h>
+#include "DataStructs.h"
+
 void compute(void *computeStruct) {
-     
+    ComputeData *cData = (ComputeData*) computeStruct;
+    *(cData->tempCorrected) = (int) floor(5 + 0.75 * (*(cData->temperatureRaw)));
+    *(cData->sysPressCorrected)  = (int) floor(9 + 2 * (*cData->systolicPressRaw));
+    *(cData->)
 }
--- Lab 2/measure.c
+++ Lab 2/measure.c
@@ -1,3 +1,28 @@
+int numOfCall = 0;
+
 void measure(void *measureStruct) {
-     
+     numOfCall++;
 }
+
+void calcTemperature(int *temperature) {
+    // cases:   0-15
+    //          15-50
+    //          50+
+    if (*temperature <= 50) {
+        if (numOfCall % 2 == 0) {
+            *temperature += 2;
+        }
+        else {
+            *temperature -= 1;
+        }
+    }
+    else {
+        if (numOfCall % 2 == 0) {
+            *temperature -= 2;
+        }
+        else {
+            *temperature += 1;
+        }
+    }
+}   
+
