2b3ea8b9b14a1c18c6c7df2dfa5523a43213ec12
Zixiang Li <lzx97@outlook.com>
2018-04-15 18:02:33 -0700

    finished compute.c

--- Lab 2/compute.c
+++ Lab 2/compute.c
@@ -4,6 +4,7 @@
 void compute(void *computeStruct) {
     ComputeData *cData = (ComputeData*) computeStruct;
     *(cData->tempCorrected) = (int) floor(5 + 0.75 * (*(cData->temperatureRaw)));
-    *(cData->sysPressCorrected)  = (int) floor(9 + 2 * (*cData->systolicPressRaw));
-    *(cData->)
+    *(cData->sysPressCorrected) = (int) floor(9 + 2 * (*cData->systolicPressRaw));
+    *(cData->diasCorrected) = (int) floor(6 + 1.5 * (*cData->diastolicPressRaw));
+    *(cData->prCorrected) = (int) floor(8 + 3 * (*cData->pulseRateRaw));
 }
