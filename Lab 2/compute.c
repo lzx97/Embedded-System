83272afab7c6091324b22233fd68feef018b6b8b
Zixiang Li <lzx97@outlook.com>
2018-04-17 16:56:44 -0700

    modified compute.c according to char data type

--- Lab 2/compute.c
+++ Lab 2/compute.c
@@ -4,8 +4,13 @@
 // Set cData values to chars
 void compute(void *computeStruct) {
     ComputeData *cData = (ComputeData*) computeStruct;
-    *(cData->tempCorrected) = (int) floor(5 + 0.75 * (*(cData->temperatureRaw)));
-    *(cData->sysPressCorrected) = (int) floor(9 + 2 * (*cData->systolicPressRaw));
-    *(cData->diasCorrected) = (int) floor(6 + 1.5 * (*cData->diastolicPressRaw));
-    *(cData->prCorrected) = (int) floor(8 + 3 * (*cData->pulseRateRaw));
+    int temp = (int) floor(5 + 0.75 * (*(cData->temperatureRaw)));
+    int systolicPres = (int) floor(9 + 2 * (*cData->systolicPressRaw));
+    int diastolicPres = (int) floor(6 + 1.5 * (*cData->diastolicPressRaw));
+    int pr = (int) floor(8 + 3 * (*cData->pulseRateRaw));
+
+    sprintf(*(cData->tempCorrected), "%d", temp);
+    sprintf(*(cData->sysPressCorrected), "%d", systolicPres);
+    sprintf(*(cData->diasCorrected), "%d", diastolicPres);
+    sprintf(*(cData->prCorrected), "%d", pr);
 }
