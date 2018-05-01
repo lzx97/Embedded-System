a03dd07f5e26c8d64650bcfc4724620aa3ccabc3
Zixiang Li <lzx97@outlook.com>
2018-04-30 20:52:05 -0700

    updated pseudocode for communicationPS

--- lab3/peripheralCom.c
+++ lab3/peripheralCom.c
@@ -1,9 +1,27 @@
 
 
-void peripheralComSC(char *data, void *dataStruct) {
+void communicationSC(char *data, void *dataStruct) {
     // TODO: implement
     if (message[1] == 'M'){
         if (message[2] == 'B')
     } else if (message[1] == 'C')
     ....
+}
+
+void communicationPS() {
+    if (available) {
+        while (s.availiable) {
+            char[] received;
+            
+            
+
+            
+        }
+        //
+
+        if (received[1] == 'M') {
+            measure();
+            Serial.write('');
+        }
+    }
 }
