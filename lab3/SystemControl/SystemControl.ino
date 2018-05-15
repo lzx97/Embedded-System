8071e9f4eb4c9a59d8139228e5ac64afcf989bc7
Zixiang Li <lzx97@outlook.com>
2018-05-14 19:27:28 -0700

    Added buffers to the SC

--- lab3/SystemControl/SystemControl.ino
+++ lab3/SystemControl/SystemControl.ino
@@ -3,7 +3,7 @@ extern "C" {
 #endif
 #include "Bool.h"
 #include "DataStructs.h"
-#include "warningSC.h"
+//#include "warningSC.h"
 //#include "schedulerSC.h"
 #include "computeSC.h"
 #ifdef __cplusplus
@@ -35,26 +35,16 @@ unsigned int displayInterval = 5;
 unsigned int warningInterval = 1;
 unsigned int statusInterval = 5;
 
-unsigned int temperatureRaw = 75;
-unsigned int systolicPressRaw = 80;
-unsigned int diastolicPressRaw = 80;
-unsigned int pulseRateRaw = 50;
+char bloodPressCorrectedBuf[48];
+char tempCorrectedBuf[32];
+char pulseRateCorrectedBuf[24];
 
-float tempNumeric = 0;
-unsigned int sysNumeric = 0;
-unsigned int diasNumeric = 0;
-unsigned int pulseNumeric = 0;
-
-unsigned int bloodPressCorrectedBuf[16] = {NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL};
-float tempCorrectedBuf[8] = {NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL};
-unsigned int pulseRateCorrectedBuf[8] = {NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL};
-
-unsigned int bloodPressRawBuf[16] = {NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL};;
-unsigned int temperatureRawBuf[8] = {NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL};
-unsigned int pulseRateRawBuf[8] = {NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL};
+char bloodPressRawBuf[40];
+char temperatureRawBuf[16];
+char pulseRateRawBuf[24];
 
 
-char **batteryState = "200";
+char batteryState[3];
 /*
 unsigned char bpOutOfRange = 0;
 unsigned char tempOutOfRange = 0;
@@ -87,39 +77,33 @@ void setup(void) {
     mData.measureInterval = &measureInterval;
     
     // Raw data
-    mData.bloodPressRawBuf = bloodPressRawBuf;
-    mData.pulseRateRawBuf = pulseRateRawBuf;
-    mData.temperatureRawBuf = temperatureRawBuf;
+    mData.bloodPressRawBuf = &bloodPressRawBuf;
+    mData.pulseRateRawBuf = &pulseRateRawBuf;
+    mData.temperatureRawBuf = &temperatureRawBuf;
     
-    mData.sysMeasureComplete = &sysMeasureComplete;
-    mData.diaMeasureComplete = &diaMeasureComplete;
-    mData.tempIncrease = &tempIncrease;
-    mData.bpIncrease = &bpIncrease;
-    mData.numOfMeasureCalls = &numOfMeasureCalls;
+    // measure selections
+    mData.tempSelection = &tempSelection;
+    mData.bpSelection = &bpSelection;
+    mData.pulseSelection = &pulseSelection;
 //    mData.MeasureTCB = &MeasureTCB;
 //    mData.ComputeTCB = &ComputeTCB;
 
 
     // Add variables to compute struct
     cData.globalTime = &globalTime;
-    cData.bloodPressRawBuf = bloodPressRawBuf;
-    cData.pulseRateRawBuf = pulseRateRawBuf;
-    cData.temperatureRawBuf = temperatureRawBuf;
+    cData.computeInterval = &computeInterval;
 
-/*
-    cData.tempNumeric = &tempNumeric;
-    cData.sysNumeric = &sysNumeric;
-    cData.diasNumeric = &diasNumeric;
-    cData.pulseNumeric = &pulseNumeric;
-*/
+    cData.tempCorrectedBuf = &tempCorrectedBuf;
+    cData.bloodPressCorrectedBuf = &bloodPressCorrectedBuf;
+    cData.prCorrectedBuf = &pulseRateCorrectedBuf;
+
+
     // Add variables to display struct
     dData.globalTime = &globalTime;
     dData.displayInterval = &displayInterval;
-    dData.bloodPressCorrectedBuf = bloodPressCorrectedBuf;
-    dData.pulseRateCorrectedBuf = pulseRateCorrectedBuf;
-    dData.tempCorrectedBuf = tempCorrectedBuf;
-    dData.bloodPressRawBuf = bloodPressRawBuf;
-    dData.pulseRateRawBuf = pulseRateRawBuf;
+    dData.bloodPressCorrectedBuf = &bloodPressCorrectedBuf;
+    dData.prCorrectedBuf = &pulseRateCorrectedBuf;
+    dData.tempCorrectedBuf = &tempCorrectedBuf;
     dData.bpHigh = &bpHigh;
     dData.bpLow = &bpLow;
     dData.tempOff = &tempOff;
@@ -130,10 +114,6 @@ void setup(void) {
     dData.tempOutOfRange = &tempOutOfRange;
     */
     dData.batteryLow = &batteryLow;
-    dData.tempNumeric = &tempNumeric;
-    dData.sysNumeric = &sysNumeric;
-    dData.diasNumeric = &diasNumeric;
-    dData.pulseNumeric = &pulseNumeric;
     dData.batteryState = &batteryState;
     dData.displayInterval = &displayInterval;
     dData.tempSelection = &tempSelection;
@@ -149,6 +129,11 @@ void setup(void) {
     // Add values to warning/alarm struct
     wData.globalTime = &globalTime;
     wData.warningInterval = &warningInterval;
+    
+    // Raw Data
+    wData.bloodPressRawBuf = &bloodPressRawBuf;
+    wData.pulseRateRawBuf = &pulseRateRawBuf;
+    wData.temperatureRawBuf = &temperatureRawBuf;
     /*
     wData.bpOutOfRange = &bpOutOfRange;
     wData.pulseOutOfRange = &pulseOutOfRange;
@@ -160,10 +145,6 @@ void setup(void) {
     wData.tempOff = &tempOff;
     wData.pulseOff = &pulseOff;
     wData.batteryLow = &batteryLow;
-    wData.tempNumeric = &tempNumeric;
-    wData.sysNumeric = &sysNumeric;
-    wData.diasNumeric = &diasNumeric;
-    wData.pulseNumeric = &pulseNumeric;
     wData.sysAlarm = &sysAlarm;
     wData.alarmAcknowledge = &alarmAcknowledge;
     wData.alarmTimer = &alarmTimer;
